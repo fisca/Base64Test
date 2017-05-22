a9808f0a4bae38aeb1c59fcc2b3011f5f8dc9885
Suttipong Srikok <suttipongs@toptrader.co.th>
2017-05-22 23:54:06 +0700

    Add base64_decode, build_decoding_table and base64_ cleanup functions

--- Base64Test/Base64Test.cpp
+++ Base64Test/Base64Test.cpp
@@ -2,6 +2,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+void build_decoding_table(void);
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -25,11 +27,12 @@ extern "C" {
 #endif
 
 //+------------------------------------------------------------------------------+
-//| Base64 Decode and Encode Source Code Copy and Modify from:			         |
+//| Base64 Encode and Decode Source Code Copy and Modify from:			         |
 //| http://stackoverflow.com/questions/342409/how-do-i-base64-encode-decode-in-c |
 //+------------------------------------------------------------------------------+
 
-static char encoding_table[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+static char encoding_table[] = 
+{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
@@ -37,7 +40,8 @@ static char encoding_table[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
 'w', 'x', 'y', 'z', '0', '1', '2', '3',
 '4', '5', '6', '7', '8', '9', '+', '/' };
-//static char *decoding_table = NULL;
+
+static char *decoding_table = NULL;
 static int mod_table[] = { 0, 2, 1 };
 
 __declspec(dllexport) char *base64_encode(const unsigned char *data,
@@ -71,5 +75,52 @@ __declspec(dllexport) char *base64_encode(const unsigned char *data,
 	return encoded_data;
 }
 
+__declspec(dllexport) unsigned char *base64_decode(const char *data,
+	size_t input_length,
+	size_t *output_length) {
 
+	if (decoding_table == NULL) build_decoding_table();
+
+	if (input_length % 4 != 0) return NULL;
+
+	*output_length = input_length / 4 * 3;
+	if (data[input_length - 1] == '=') (*output_length)--;
+	if (data[input_length - 2] == '=') (*output_length)--;
+
+	unsigned char *decoded_data = (unsigned char*)malloc(*output_length);
+	if (decoded_data == NULL) return NULL;
+
+	for (int i = 0, j = 0; i < (int)input_length;) {
+
+		uint32_t sextet_a = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+		uint32_t sextet_b = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+		uint32_t sextet_c = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+		uint32_t sextet_d = data[i] == '=' ? 0 & i++ : decoding_table[data[i++]];
+
+		uint32_t triple = (sextet_a << 3 * 6)
+			+ (sextet_b << 2 * 6)
+			+ (sextet_c << 1 * 6)
+			+ (sextet_d << 0 * 6);
+
+		if (j < (int)*output_length) decoded_data[j++] = (triple >> 2 * 8) & 0xFF;
+		if (j < (int)*output_length) decoded_data[j++] = (triple >> 1 * 8) & 0xFF;
+		if (j < (int)*output_length) decoded_data[j++] = (triple >> 0 * 8) & 0xFF;
+	}
+
+	return decoded_data;
+}
+
+
+void build_decoding_table(void) {
+
+	decoding_table = (char*)malloc(256);
+
+	for (int i = 0; i < 64; i++)
+		decoding_table[(unsigned char)encoding_table[i]] = i;
+}
+
+
+void base64_cleanup(void) {
+	free(decoding_table);
+}
 
